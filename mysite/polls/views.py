bf13f21b15c73ec9cf644e1210354a52eca98a9b
Alexy_Yohko <47605546+olexapro@users.noreply.github.com>
2019-12-06 20:42:44 +0300

    1.1

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
